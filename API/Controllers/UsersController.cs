f1c26e8c186c9825796368ad39ce403bcf652482
Raphael Aubin <raubin@dlaubin.ca>
2021-01-07 19:48:44 -0500

    Add angular

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -21,7 +21,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.ToListAsync(); 
         }
 
         // api/users/3
