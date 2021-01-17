097f38331fac5665cb0fcb9cc8aead2d15d70836
Raphael Aubin <raubin@dlaubin.ca>
2021-01-16 20:58:10 -0500

    End of section 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { User } from '../_model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
+  
   private currentUserSource = new ReplaySubject<User>(1); //Kind of a buffer - 1 is size of the buffer
   currentUser$ = this.currentUserSource.asObservable(); //$ to show its an observable
 
