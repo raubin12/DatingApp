e53ddbd4fdd8a0f21ba56c594adbb786fcb94cf9
Raphael Aubin <raubin@dlaubin.ca>
2021-01-25 20:38:03 -0500

    End of section 12

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css']
+})
+
+//Purpose of this class if to been called like this: username: new FormControl('', Validators.required), but with TextInputComponent
+//instead of FormControl
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() label: string;
+  @Input() type = 'text';
+
+  //self contained
+  constructor(@Self() public ngControl: NgControl) { 
+    this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void { }
+
+  registerOnChange(fn: any): void { }
+
+  registerOnTouched(fn: any): void { }
+}
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,21 +11,57 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
+  maxDate: Date;
+  validationErrors: string[] = []; //Initialize it with empty
 
-  model: any = {};
-
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, 
+              private toastr: ToastrService, 
+              private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initializeForm() {
+    // this.registerForm = new FormGroup({
+    //   //username: new FormControl('Hello'), //With default value
+    //   username: new FormControl('', Validators.required),
+    //   password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]) //password is the name of the FormControl to compareTo
+    // });
+
+    //With the FormBuilder service
+    this.registerForm = this.fb.group({
+      gender: ['male'], //Radio button
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]] //password is the name of the FormControl to compareTo
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      //control over here is the control using this function as a custom validator
+      //matchTo is the name of the FormControl to compareTo
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMathching: true} //isMatching is a custom name, it need an object as a return value
+    };
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
-      this.cancel();
+    //console.log(this.registerForm.value);
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
     }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
+      this.validationErrors = error;
     });
   }
 
